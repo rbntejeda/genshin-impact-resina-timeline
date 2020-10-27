6f0a197cdfb2f50d98e82bc17dc1a75ca079269f
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2020-10-27 00:43:27 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
 
+import localeEnCL from '@angular/common/locales/es-CL';
+registerLocaleData(localeEnCL,'es-CL')
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide:LOCALE_ID,
+    useValue:"es-CL"
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/timeline/timeline.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from "moment";
+
+
+@Component({
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.scss']
+})
+export class TimelineComponent implements OnInit {
+  currentResina;
+
+  timeline: {
+    time: number,
+    sum: number,
+    total: number,
+    cuenta: string
+  }[] = []
+
+  int
+
+  constructor() {
+    if (localStorage['currentResina']) {
+      this.currentResina = +localStorage.currentResina
+      this.calculate()
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  calculate() {
+    this.timeline = []
+    console.log("calcular")
+    localStorage.setItem('currentResina', this.currentResina)
+
+    var time = Date.now();
+    var sum = 0;
+    var total = this.currentResina;
+    var cuenta = "-";
+
+    this.timeline.push({ time, sum, total, cuenta })
+
+    var restoResina = this.currentResina % 20;
+    var diffResina = 20 - restoResina;
+
+    time += diffResina * 1000 * 60 * 8;
+    sum += diffResina;
+    total += diffResina;
+    cuenta = this.duration(time);
+
+    this.timeline.push({ time, sum, total, cuenta })
+    while (total <= 100) {
+      time += 20 * 1000 * 60 * 8;
+      sum += 20;
+      total += 20;
+      cuenta = this.duration(time);
+      this.timeline.push({ time, sum, total, cuenta })
+    }
+    if (this.int) {
+      clearInterval(this.int)
+    }
+    this.int = setInterval(() => {
+      this.updateCuenta()
+    }, 1000)
+    console.log({ time, restoResina, diffResina })
+
+  }
+
+  duration(future) {
+    var n = (n) => {
+      return n > 9 ? "" + n : "0" + n;
+    }
+    var duration = moment.duration(future - Date.now())
+    var hours = duration.hours();
+    var minutes = duration.minutes();
+    var second = duration.seconds();
+    var textHour = (hours > 9)
+    return `${n(hours)}:${n(minutes)}:${n(second)}`;
+  }
+
+  updateCuenta() {
+    this.timeline.forEach(e => {
+      if (e.time > Date.now()) {
+        e.cuenta = this.duration(e.time);
+      } else {
+        e.cuenta = "-";
+      }
+    });
+  }
+
+}
